c0a96ace29551db99d92133dd50f9d3bc816403b
Naresh Ediga <Naresh Ediga@Jagadeesh>
2022-09-23 17:51:26 +0530

    updated 1

--- src/test/java/Practice/Sample1.java
+++ src/test/java/Practice/Sample1.java
@@ -4,14 +4,11 @@ import org.testng.annotations.Test;
 
 public class Sample1 {
 	
-	@Test(groups = "regression")
+	@Test
 	public void step1Test()
 	{
-		String browser = System.getProperty("b");
-		String url = System.getProperty("u");
-		
-		System.out.println("browser name is ******>>>>>  "+browser);
-		System.out.println("url is ******>>>>>  "+url);
+				System.out.println("browser");
+		System.out.println("url ");
 		
 	
 		
